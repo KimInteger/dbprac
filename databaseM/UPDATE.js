7f86300c77456073e7ce66d6fd5d1ca59d34de62
KimInteger <kjs20151240@gmail.com>
2024-06-20 19:39:33 +0900

    module화

--- databaseM/UPDATE.js
+++ databaseM/UPDATE.js
@@ -1,15 +1,19 @@
-const sqlite3 = require('sqlite3').verbose();
-
-const db = new sqlite3.Database('../database/product.db');
-
-db.serialize(()=>{
-
-  const stmt = db.prepare(`UPDATE product SET count =? WHERE name =?`);
-
-  stmt.run(1,'드래곤볼');
+function update(count,name){
+  const sqlite3 = require('sqlite3').verbose();
   
-  stmt.finalize();
+  const db = new sqlite3.Database('../database/product.db');
 
-});
+  db.serialize(()=>{
+  
+    const stmt = db.prepare(`UPDATE product SET count =? WHERE name =?`);
+  
+    stmt.run(parseInt(count,10),name);
+    
+    stmt.finalize();
+  
+  });
+  
+  db.close();
+}
 
-db.close();
+module.exports = update;
