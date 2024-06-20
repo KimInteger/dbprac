4d5f5a04ec466804295aa9771ef0838e67ee9dd8
KimInteger <kjs20151240@gmail.com>
2024-06-20 21:05:29 +0900

    update모듈 변경

--- databaseM/UPDATE.js
+++ databaseM/UPDATE.js
@@ -1,8 +1,8 @@
 function update(count,name){
   const sqlite3 = require('sqlite3').verbose();
   
-  const db = new sqlite3.Database('../database/product.db');
-
+  const db = new sqlite3.Database('./database/product.db');
+  
   db.serialize(()=>{
   
     const stmt = db.prepare(`UPDATE product SET count =? WHERE name =?`);
@@ -14,6 +14,7 @@ function update(count,name){
   });
   
   db.close();
+  
 }
 
 module.exports = update;
