8833fc11b51abb6fba36a71774e6b1f5beaa9593
KimInteger <kjs20151240@gmail.com>
2024-06-20 16:01:47 +0900

    테이블 생성 코드 작성

--- databaseM/CREATE.js
+++ databaseM/CREATE.js
@@ -2,3 +2,15 @@ const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('../database/product.db');
 
+db.serialize((err)=>{
+  if(err){
+    return new Error("에러가 발생", err.message);
+  }
+  db.run('CREATE TABLE IF NOT EXISTS product (id INTEGER PRIMARY KEY, name TEXT NOT NULL, count INTEGER NOT NULL, price INTEGER NOT NULL)',(err)=>{
+    if (err){
+      return new Error(err.message);
+    }
+  });
+  
+  
+});
